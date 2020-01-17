eeeedbc76a46e4547542ca663911608123b4f7dc
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2020-01-17 12:33:30 -0300

    Add Company.vue refactor

--- src/pages/Company.vue
+++ src/pages/Company.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="company__wrapper">
     <div class="text">
-      <h2>PAGE</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra
-      sagittis porta. Donec eu magna lobortis, interdum nibh non, scelerisque
-      lorem.
+      <h2>PAGE</h2>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra
+        sagittis porta. Donec eu magna lobortis, interdum nibh non, scelerisque
+        lorem.
+      </div>
     </div>
   </div>
 </template>
